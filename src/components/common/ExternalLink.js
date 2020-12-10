f214e3b4c19623acd60d7e75420dbafeeb26891c
Dezso Szabo <dexirta@hotmail.com>
2020-12-10 21:46:17 +0100

    outbound links for analytics

--- src/components/common/ExternalLink.js
+++ src/components/common/ExternalLink.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {OutboundLink} from 'gatsby-plugin-google-analytics'
 
 const ExternalLink = ({href, children, ...other}) => (
-  <a href={href} {...other} rel="noreferrer noopener" target="_blank">
+  <OutboundLink href={href} {...other} rel="noreferrer noopener" target="_blank">
     {children}
-  </a>
+  </OutboundLink>
 )
 
 export default ExternalLink
