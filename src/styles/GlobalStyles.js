5b0f0380ef24052cdc8bc30d90de212a0881f563
Dezso Szabo <dexirta@hotmail.com>
2020-12-11 20:50:39 +0100

    preload fonts fix

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -5,7 +5,7 @@ const normalize = `
 `
 
 const GlobalStyles = createGlobalStyle`
- @import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@500;800&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@500;800&display=swap');
   ${normalize};
 
   body {
