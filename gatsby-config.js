bcf30a5b0b8d6a30c1c6c238e4242d17b165bccc
Dezso Szabo <dexirta@hotmail.com>
2020-12-13 19:43:31 +0100

    csp fine tune and removed from meta's

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,21 +80,5 @@ module.exports = {
         defer: true,
       },
     },
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
-        mergeScriptHashes: false, // you can disable scripts sha256 hashes
-        mergeStyleHashes: false, // you can disable styles sha256 hashes
-        mergeDefaultDirectives: true,
-        directives: {
-          'connect-src': "'self' www.google-analytics.com 'unsafe-inline'",
-          'script-src': "'self' www.google-analytics.com 'unsafe-inline'",
-          'style-src': "'self' 'unsafe-inline'",
-          'img-src': "'self' data: www.google-analytics.com 'unsafe-inline'",
-        },
-      },
-    },
   ],
 }
