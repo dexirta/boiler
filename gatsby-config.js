68687ce8f958cc61f81c43a707c4d58481807936
Dezso Szabo <dexirta@hotmail.com>
2020-12-13 17:54:11 +0100

    csp options test 7

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,7 +89,7 @@ module.exports = {
         mergeStyleHashes: false, // you can disable styles sha256 hashes
         mergeDefaultDirectives: true,
         directives: {
-          'connect-src': "www.google-analytics.com 'unsafe-inline'",
+          'connect-src': "'self' www.google-analytics.com 'unsafe-inline'",
           'script-src': "'self'",
           'style-src': "'self' 'unsafe-inline'",
           'img-src': "'self' 'unsafe-inline' data: www.google-analytics.com 'unsafe-inline'",
